dc1e5e760f929629f4f939cb0c155284d6985c22
Anna <ania.posacka@gmail.com>
2018-02-17 13:34:31 -0800

    Adding scripts for Het bacteria versus phytoplankton Cu quota comparions

---
+++ Figures/Fig.5_Cu-quotas-vs-inorg-Cu-synthesis-Aquil.R
@@ -0,0 +1,74 @@
+# script to generate Fig.4 and 5 in the Frontiers manuscript. This figure compares quotas of 
+# phytoplankton and geterotrophic bacteria as a function of inorganic Cu (cultured in Aquil)
+
+
+library(tidyverse)
+library(cowplot)
+
+data <-read_csv("C:/Users/Ania/Bact_Stoichiometry/Frontiers/Copper_to-carbon-ratios-compilation/Copper_to-carbon-synthesis-Aquil.csv") 
+
+#glimpse(data)
+
+data$Taxon<- as.factor(data$Taxon)
+data$Species<- as.factor(data$Species)
+data$Domain<- as.factor(data$Domain)
+data$Strategy <- as.factor(data$Strategy)
+
+#-------Fig 4------------------------
+
+#~~~~~~~Fig 5~~~~~~~~~~~~~~~~~~~~~~~~
+
+phytos<-data%>%
+	filter(Strategy=="Photosynthetic",Cu_C<13)%>%
+	ggplot(aes(x=Cu_prime_log,y=Cu_C,fill=Taxon))+
+	geom_jitter(shape=21,size=5,alpha=1/2, width=0.02)+
+	annotate("rect", xmin=14.5, xmax=12, ymin=2.8, ymax=4.9, alpha=.3, fill="gray")+
+	annotate("segment", x = 13.74, xend = 12, y = 6, yend = 6,
+					 arrow=arrow(ends="both", angle=90,length=unit(.2,"cm")))+
+	annotate("text", x = 12.8, y = 7.0, label="log [Cu'] range in seawater", size=4.5) +
+	annotate("text", x = 14.0, y = 12.5, label="Phytoplankton", fontface = 2,size=4.5) +
+	scale_x_reverse(name="-log([Cu'])",expand=c(0,0))+
+	scale_y_continuous(breaks = c(0,1.5,3,4.5,6,7.5,9,10.5,12),limits = c(0,12.6))+
+	ylab(expression("Cu:C" ~(mu~mol:mol)))+
+	geom_hline(data = data, aes(yintercept = 2.42),linetype="dashed",size=0.8)+
+	geom_hline(data = data, aes(yintercept = 1.83),linetype="dotted",size=0.8)+
+	theme_bw()+
+	theme(panel.grid.major.x = element_blank(), 
+				panel.grid.minor.x = element_blank(),
+				panel.grid.major.y = element_blank(),
+				panel.grid.minor.y = element_blank(),
+				axis.title = element_text(size=14),
+				axis.text = element_text(size=14),
+				legend.text = element_text(size=10),
+				legend.position = "none")
+
+
+bact<-data%>%
+	filter(Strategy=="Heterotrophic")%>%
+	ggplot(aes(x=Cu_prime_log,y=Cu_C,fill=Taxon))+
+	geom_jitter(shape=23, size=5,alpha=1/2)+
+	annotate("rect", xmin=14.5, xmax=12, ymin=2.8, ymax=4.9, alpha=.3, fill="gray")+
+	annotate("segment", x = 13.74, xend = 12, y = 6, yend = 6,
+					 arrow=arrow(ends="both", angle=90,length=unit(.2,"cm")))+
+	annotate("text", x = 12.8, y = 7.0, label="log [Cu'] range in seawater", size=4.5) +
+	annotate("text", x = 13.5, y = 12.5, label="Heterotrophic bacteria", fontface = 2,size=4.5) +
+	scale_x_reverse(name="-log([Cu'])",expand=c(0,0))+
+	ylab(expression("Cu:C" ~(mu~mol:mol)))+
+	scale_y_continuous(breaks = c(0,1.5,3,4.5,6,7.5,9,10.5,12),limits = c(0,12.6))+
+	geom_hline(data = data, aes(yintercept = 1.24),linetype="dashed",size=0.8)+
+	theme_bw()+
+	guides(color=guide_legend(nrow =2))+
+	theme(panel.grid.major.x = element_blank(), 
+				panel.grid.minor.x = element_blank(),
+				panel.grid.major.y = element_blank(),
+				panel.grid.minor.y = element_blank(),
+				axis.title = element_text(size=14),
+				axis.text = element_text(size=14),
+				legend.text = element_text(size=10),
+				legend.position = "none")
+
+
+plot <-plot_grid(phytos,bact,
+								 labels = c("A", "B"),label_size = 12, align = "h")
+
+save_plot (filename="Bact_vs_Phytos.tiff", plot= plot, base_height= 4, base_width =9.5)
---
+++ Figures/Fig4_Cu-quotas-bact-vs-euk-algae.R
@@ -0,0 +1,45 @@
+# script to produce Fig. 4 of the Frontiers manuscript with statistical tests
+
+
+# load the data from csv
+library(tidyverse)
+data <-read_csv("C:/Users/Ania/Bact_Stoichiometry/Frontiers/Copper_to-carbon-ratios-compilation/Cu_quotas-synthesis-Aquil.csv") 
+glimpse(data)
+
+# sort out the variable formats
+data$Strategy <- as.factor(data$Strategy)
+data$Taxon<- as.factor(data$Taxon)
+data$Species<- as.factor(data$Species)
+data$Domain<- as.factor(data$Domain)
+
+# check names for Domains
+#levels(data$Domain)
+# are there duplicated values at Cu_C>5?
+#ata%>%select(Ref,Domain,Species,Cu_C)%>%filter(Cu_C>5)
+
+p1 <- data%>%
+	filter(Cu_C<15, Domain %in% c("Euk_algae","Het_bacteria"))%>%
+	ggplot(aes(x = Domain, y = Cu_C,fill=Domain))+
+	geom_boxplot()+
+#	geom_jitter(width=0.05)+
+	scale_fill_manual(values=c("#00AFBB", "#E7B800"))+
+	ylab(expression("Cu:C" ~(mu~mol:mol)))+
+	ylim(0,12.5)+
+	theme_bw()+
+	theme(panel.grid.major.x = element_blank(), 
+				panel.grid.minor.x = element_blank(),
+				panel.grid.major.y = element_blank(),
+				panel.grid.minor.y = element_blank(),
+				axis.title.y= element_text(size=12),
+				axis.title.x= element_blank(),
+				axis.text.x = element_text(angle = 25, hjust = 0.9),
+				axis.text = element_text(size=12),
+				legend.text = element_text(size=10),
+				legend.position = "none")
+
+save_plot (filename="Fig4_Bact_vs_Phytos.tiff", plot= plot, base_height= 4, base_width =9.5)
+
+#-----------------------------STATISTICAL ANALYSIS---------------------------------------------
+
+
+
