d9aa67ff9f12bce47b84cf62f275634fa02e9bbc
Anna <ania.posacka@gmail.com>
2018-05-16 16:02:36 -0700

    reorganizing stats folder and scripts, anovas went below of descriptive stats

--- Statistics/01_bact-growth-rates-stats.R
+++ Statistics/01_bact-growth-rates-stats.R
@@ -1,8 +1,36 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~README~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# This script produces runs one way Analysis of Variance to test the effect of
-# Cu level in growth media on growth rates of 4 strains of marine henetrotrophic bacteria
-#- this data appears in Table 4 of Frontiers in Marine Science manuscript. 
+# This script produces summary statistics (mean, stdev, stderror and number of 
+# observations) for growth rates at various Cu levels 
+# in 4 strains of marine henetrotrophic bacteria - this data appears in 
+# in Fig.1 and Table 1 of Frontiers in Marine Science manuscript. 
 # The results are written to a csv file
+
+
+library(tidyverse)
+
+# load the data
+growth <-read_csv("Data/01_Bact-growth-rates-tidydata.csv")
+
+growth$Strain <- as.factor(growth$Strain)
+growth <- na.omit(growth)
+
+# calculating summary statistis using commands from the dplyr package
+Descr_stats <-growth %>%
+	group_by(Strain,Cu_total)%>%
+	summarise(mean_gr =mean(mu_day, na.rm= TRUE), 
+						stdev=sd(mu_day, na.rm= TRUE),
+						SEM= stdev/sqrt(n()),
+						(n()))
+
+# arranging the data in the order of Strain and Cu level
+Descr_stats <- Descr_stats %>%
+	arrange(Strain,Cu_total)
+
+write_csv(Descr_stats,"01_bact-growth-descr-stats.csv")
+
+#----------------------------------------
+# ANOVA
+#----------------------------------------
 
 library(purrr)
 library(broom)
@@ -63,3 +91,5 @@ Bartlett_results <-data_nested %>%
 ANOVA_results
 
 write_csv(ANOVA_results,"01_bact-growth-anova-bartlett-results.csv")
+
+
