0dc9ad35c7cddbcec9a836d8e9471a37f730c6df
Anna <ania.posacka@gmail.com>
2018-05-16 15:36:37 -0700

    moved anova to its own script

--- Statistics/Descriptive_stats/01_bact-growth-rates-summary-stats.R
+++ Statistics/Descriptive_stats/01_bact-growth-rates-summary-stats.R
@@ -12,6 +12,7 @@ library(tidyverse)
 growth <-read_csv("Data/01_Bact-growth-rates-tidydata.csv")
 
 growth$Strain <- as.factor(growth$Strain)
+growth <- na.omit(growth)
 
 # calculating summary statistis using commands from the dplyr package
 Descr_stats <-growth %>%
@@ -27,55 +28,4 @@ Descr_stats <- Descr_stats %>%
 
 write_csv(Descr_stats,"01_bact-growth-descr-stats.csv")
 
-#~~~~~~~~~~~~~~~~~~ANOVA~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-data_nested <- growth %>%
-	group_by(Strain) %>%
-	nest()
-
-# checking what the new dataset looks like
-# by examining the data in the first grouped element
-# of the df
-# data_nested[[1,"data"]]
-
-
-# create a function for ANOVA analysis and Bartlett test for 
-# equal variances in order to assess if ANOVA assumptions are held
-
-ANOVA <- function(df) {
-	aov(mu_day~Cu_total, data=df)
-}
-
-Bartlett <- function(df) {
-	bartlett.test(mu_day~Cu_total, data=df)
-}
-
-# Applying ANOVA and Bartlett functions to the nested data
-data_nested <-data_nested %>%
-	mutate(nested_ANOVA=map(data,ANOVA))
-
-data_nested <-data_nested %>%
-	mutate(nested_Bartlett=map(data,Bartlett))
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Extracting F and p values using BROOM
-# tidy(data_nested$nested_ANOVA[[1:3]])
-
-data_nested <- data_nested %>%
-	mutate(tidy_ANOVA= map(nested_ANOVA,tidy))
-
-data_nested <- data_nested %>%
-	mutate(tidy_Bartlett= map(nested_Bartlett,tidy))
-
-# Creating a statistics tibble
-ANOVA_results <- data_nested %>%
-	select(Strain, tidy_ANOVA) %>%
-	unnest(tidy_ANOVA)
-
-Bartlett_results <-data_nested %>%
-	select(Strain, tidy_Bartlett) %>%
-	unnest(tidy_Bartlett)
-
-ANOVA_results
-
-write_csv(ANOVA_results,"GR-ANOVA-Bartlett-results.csv")
