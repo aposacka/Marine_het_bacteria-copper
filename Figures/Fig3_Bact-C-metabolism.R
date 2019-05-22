752f7710c056c536c1a864a44da9ef0314eeae7a
Anna <ania.posacka@gmail.com>
2019-05-22 14:33:58 -0700

    changed sizes of axis labels

--- Figures/Fig3_Bact-C-metabolism.R
+++ Figures/Fig3_Bact-C-metabolism.R
@@ -8,7 +8,7 @@
  library(cowplot)
 
 # loading data
- data <-read_csv("Data/03_Bact-carb-metab-tidydata.csv")
+data <-read_csv("Data/03_Bact-carb-metab-tidydata.csv")
  
 # convert some variables to factors
 data$Cu_level <- as.factor(data$Cu_level)
@@ -16,13 +16,15 @@ data$Strain<- as.factor(data$Strain)
 data$Category <- as.factor(data$Category)
 
 glimpse(data)
+levels(data$Category)
+
  
 # setting the order of factors for plotting
 data$Strain<- factor(data$Strain, levels = c("Dokd_P16",
 																						 "R.pomeroyi",
 																						 "PAlt_P26"))
  
-data$Strain<- factor(data$Category, levels = c("BR_per_day",
+data$Category<- factor(data$Category, levels = c("BR_cell_day",
                                               "BP",
                                               "BCD",
                                               "BGE"))
@@ -39,10 +41,7 @@ data$Strain<- factor(data$Category, levels = c("BR_per_day",
 	    geom_point(alpha = 1/3,size = 3)+
 	    facet_wrap(~Strain, scales = "free_y")+
       scale_color_manual(values = c("#009E73","#666666","#0072B2"))+
-     # annotate ("rect", limits = c(min_xy, max_xy), alpha = .3, fill = "gray")+
-    #  annotate("rect", xmin=1.8, xmax=3.3, ymin=0.0, ymax=600, alpha=.3, fill="gray")+
-    #  scale_y_continuous(expand=c(0,0))+
-	   ylab(expression("BR"[cell]~(fmol~O[2]~cell^{-1})))+
+	   ylab(expression("BR"[cell]~(fmol~O[2]~cell^{-1}~d^{-1})))+
 	    guides(colour = FALSE)+
       theme_bw()+
       theme(legend.position = "none",
@@ -52,9 +51,9 @@ data$Strain<- factor(data$Category, levels = c("BR_per_day",
          panel.grid.minor.x = element_blank(),
          panel.grid.major.y = element_blank(),
          panel.grid.minor.y = element_blank(),
-         axis.text = element_text(size=11),
-         axis.title.y = element_text(size=11),
-         legend.text = element_text(size=11))
+         axis.text = element_text(size=9),
+         axis.title.y = element_text(size=9),
+         legend.text = element_text(size=9))
 	   
 
 p2 <-data%>%
@@ -77,9 +76,9 @@ p2 <-data%>%
         panel.grid.minor.x = element_blank(),
         panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank(),
-        axis.text = element_text(size=11),
-        axis.title.y = element_text(size=11),
-        legend.text = element_text(size=11))
+        axis.text = element_text(size=9),
+        axis.title.y = element_text(size=9),
+        legend.text = element_text(size=9))
 
 p3 <-data%>%
   filter(Category == "BCD")%>%
@@ -101,9 +100,9 @@ p3 <-data%>%
         panel.grid.minor.x = element_blank(),
         panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank(),
-        axis.text = element_text(size=11),
-        axis.title.y = element_text(size=11),
-        legend.text = element_text(size=11))
+        axis.text = element_text(size=9),
+        axis.title.y = element_text(size=9),
+        legend.text = element_text(size=9))
 
 p4 <- data%>%
   filter(Category == "BGE")%>%
@@ -116,7 +115,7 @@ p4 <- data%>%
   # annotate ("rect", limits = c(min_xy, max_xy), alpha = .3, fill = "gray")+
   #  annotate("rect", xmin=1.8, xmax=3.3, ymin=0.0, ymax=600, alpha=.3, fill="gray")+
   #  scale_y_continuous(expand=c(0,0))+
-  ylab("BEG")+
+  ylab("BGE")+
   theme_bw()+
   theme(legend.position = "none",
         strip.text= element_blank(),
@@ -125,10 +124,11 @@ p4 <- data%>%
         panel.grid.minor.x = element_blank(),
         panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank(),
-        axis.text = element_text(size=11),
-        axis.title.y = element_text(size=11),
-        legend.text = element_text(size=11))
+        axis.text = element_text(size=9),
+        axis.title.y = element_text(size=9),
+        legend.text = element_text(size=9))
   
+
 #--------------------------------------------------
 # arranging plots for the final figure + saving,
 # both commands are from the cowplot package
